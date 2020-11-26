b9306581de2e3d854349f064afb0ff0e50656f85
Jonathan Meibergen <jonathanmeibergen@outlook.com>
2020-11-26 12:51:20 +0100

    Add project files.

---
+++ Animals/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Animals
+{
+    public abstract class Animal
+    {
+        protected int _energy = 100;
+        protected string _name;
+        public abstract string Name { get; }
+        public abstract int Energy { get; set; }
+
+        public Animal(string name)
+        {
+            _name = name; 
+        }
+        public virtual void Eat() {
+            Energy += 25;
+        }
+    }
+}
---
+++ Animals/Monkey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Animals
+{
+    public class Monkey : Animal
+    {
+        public override string Name => _name;
+        public override int Energy { get => _energy;
+                                     set => _energy = value; }
+
+        public Monkey(string name) : base(name)
+        {}
+    }
+}
